5aa6c7e528a2d3c5bb80794cbd9118d5b7fab0db
Cwekii <dominik.cvetkovski@gmail.com>
2019-05-08 08:50:34 +0200

    Timer

---
+++ Vjezbe na nastavi/Timer/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [Header("Upiši broj minuta za razinu:")]
+    public float minute; //Koliko hoćemo da traje igra u minutama
+
+    public Text time_text; //Text na kojem se prikazuje vrijeme
+
+    void Start() // Pretavaramo minute u sekunde
+    {
+        minute = minute * 60;
+    }
+
+    public void Update()
+    {
+        minute -= Time.deltaTime; //Oduzimamo vrijeme
+
+        if(minute >= 0) //Ako je veće od 0 sekundi
+        {
+            float minutes = Mathf.FloorToInt(minute / 60); //Djelimo da dobijemo cijeli broj
+            float seconds = Mathf.FloorToInt(minute % 60); //Djelimo da dodbijemo ostatak
+            if(minutes < 10 && seconds < 10) //Ako je manje od 10 sekundi/minuta da imamo 0 ispred radi ljepšeg zapisa
+            {
+                time_text.text = "0" + minutes + ":" + "0" + seconds; 
+            }
+            else if (minutes < 10 && seconds > 10) //Sekunde su u ovom slučaju dvoznamenkaste
+            {
+                time_text.text = "0" + minutes + ":" + seconds;
+            }
+            else //Prikazuj dvoznamenkaste minute i sekunde
+            {
+                time_text.text = minutes + ":" + seconds;
+            }
+        }
+    }
+}
